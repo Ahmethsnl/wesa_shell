126ae5f6d2857c060243acfb6e36b13595773755
Ahmet H. Şanlı <87249395+Ahmethsnl@users.noreply.github.com>
2025-06-30 16:15:34 +0300

    Update wesa.sh

--- wesa.sh
+++ wesa.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+# Scriptlerin tam path'leri
+wclean='bash /home/ahmsanli/WClean/WesaClean.sh'
+wpom='bash /home/ahmsanli/pomodoro.sh'
+wstart='bash /home/ahmsanli/.project_start/project_start.sh'
+
 echo -e "\n👋 Merhaba Wesa, hangi aracı çalıştırmak istersin?"
-echo "1) WCleaner  🧹  (wclean)"
-echo "2) WPomodoro ⏱️  (wpom)"
-echo "3) ProjectStarter 🏗️ (wstart)"
+echo "1) WCleaner  🧹"
+echo "2) WPomodoro ⏱️"
+echo "3) ProjectStarter 🏗️"
 echo "0) Çıkış 🚪"
-read -p "Seçimin [1-3]: " choice
+read -p "Seçimin [0-3]: " choice
 
 case "$choice" in
     1)
         echo -e "\n🧹 WCleaner çalıştırılıyor...\n"
-        eval "$(alias wclean)" && wclean
+        eval "$wclean"
         ;;
     2)
         echo -e "\n⏱️ WPomodoro çalıştırılıyor...\n"
-        eval "$(alias wpom)" && wpom
+        eval "$wpom"
         ;;
     3)
         echo -e "\n🏗️ ProjectStarter çalıştırılıyor...\n"
-        eval "$(alias wstart)" && wstart
+        eval "$wstart"
         ;;
     0)
         echo "👋 Görüşürüz Wesa!"
